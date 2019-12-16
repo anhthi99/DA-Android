7675f60bace83baa8e5a2405871393d641c472b6
LuanNguyen <kesanbauvat9@gmail.com>
2019-12-16 23:27:49 +0700

    Change Avatar
    Almost done !!

--- app/src/main/java/com/example/gametraloicauhoi_android/NetworkUtils.java
+++ app/src/main/java/com/example/gametraloicauhoi_android/NetworkUtils.java
@@ -4,13 +4,17 @@ import android.net.Uri;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -115,5 +119,49 @@ public class NetworkUtils {
             return null;
         }
         return builder.toString();
+    }
+    public static String postRequest(String uri, HashMap<String, String> postDataParams) {
+        String jsonString = null;
+        URL url;
+        try{
+            url = new URL(BASE_URL+uri);
+            HttpURLConnection conn =(HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            OutputStream os =conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+            writer.write(getPostDataString(postDataParams));
+            writer.flush();
+            writer.close();
+            os.close();
+            int responseCode = conn.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                jsonString = convertToString(conn.getInputStream());
+            } else {
+                jsonString ="{'success':false}";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Log.d(LOG_TAG,jsonString);
+        return jsonString;
+    }
+    private static String getPostDataString(HashMap<String,String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first =true;
+        for (Map.Entry<String,String> entry : params.entrySet()) {
+            if(first)
+                first = false;
+            else
+                result.append("&");
+            result.append(URLEncoder.encode(entry.getKey(),"UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(),"UTF-8"));
+        }
+        return result.toString();
     }
 }
--- app/src/main/java/com/example/gametraloicauhoi_android/QuanLyTaiKhoan.java
+++ app/src/main/java/com/example/gametraloicauhoi_android/QuanLyTaiKhoan.java
@@ -1,14 +1,132 @@
 package com.example.gametraloicauhoi_android;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.util.Base64;
+import android.util.Base64OutputStream;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.net.URL;
+import java.util.HashMap;
 
 public class QuanLyTaiKhoan extends AppCompatActivity {
-
+    public static final String UPLOAD_KEY = "hinh_anh";
+    public static final String UPLOAD_URL = "upload";
+    final int PICK_IMAGE_REQUEST = 1;
+    private Uri filePath;
+    private Bitmap bitmap;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quan_ly_tai_khoan);
+        imageView = findViewById(R.id.imgAnhDaiDien);
+    }
+    public String encodeBitmapToString(Bitmap bmp) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG,100,baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodeimage = Base64.encodeToString(imageBytes,Base64.DEFAULT);
+        return encodeimage;
+    }
+    public String getFileName(Uri uri){
+        String result = null;
+        if(uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri,null,null,null,null);
+            try {
+                if (cursor != null && cursor.moveToNext()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if ( result == null ) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut+1);
+            }
+        }
+        return result;
+    }
+    public void uploadImage(View view){
+        class UploadImage extends AsyncTask<Bitmap,Void,String> {
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(QuanLyTaiKhoan.this,"Uploading image","Please wait ...",true,true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                URL url = null;
+                try {
+                    JSONObject jsonObject = new JSONObject(s);
+                    String hinh = jsonObject.getString("hinh_anh");
+                    Log.d("HINHANH",hinh);
+                    Picasso.get().load(hinh).into(imageView);
+                } catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            protected String doInBackground(Bitmap... bitmaps) {
+                Bitmap bitmap = bitmaps[0];
+                String uploadImage = encodeBitmapToString(bitmap);
+                HashMap<String,String> data = new HashMap<>();
+                data.put(UPLOAD_KEY,uploadImage);
+                data.put("ten_hinh",getFileName(filePath));
+                String result = NetworkUtils.postRequest(UPLOAD_URL,data);
+                return result;
+            }
+        }
+        UploadImage ui = new UploadImage();
+        ui.execute(bitmap);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null ) {
+            filePath = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),filePath);
+                imageView.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void chooseImage(View view){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select image"),PICK_IMAGE_REQUEST);
     }
 }
